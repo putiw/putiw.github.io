38cd8d20cf8ee666554b273253898407af42ecb3
putiw <70220464+putiw@users.noreply.github.com>
2022-09-20 00:47:57 -0400

    upload

---
+++ test-site/scripts/main.js
@@ -0,0 +1,38 @@
+const myImage = document.querySelector('img');
+
+myImage.onclick = () => {
+    const mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/winterHouse.JPG'){
+
+        myImage.setAttribute('src','images/blenderHouse.png');
+    } else  {
+        myImage.setAttribute('src','images/winterHouse.JPG');
+
+    }
+}
+
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+
+function setUserName() {
+    const myName = prompt('Please enter your name.');
+    if (!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem('name', myName);
+    myHeading.textContent = `Welcome, ${myName}`;
+  }
+}
+  
+  if (!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    const storedName = localStorage.getItem('name');
+    myHeading.textContent = `Welcome, ${storedName}`;
+  }
+  
+  myButton.onclick = () => {
+    setUserName();
+  }
