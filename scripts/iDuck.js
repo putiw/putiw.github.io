6c02de39e560d362403c6ebbce5a9deca58d9fe4
putiwen <pw1246@nyu.edu>
2022-09-21 19:44:45 -0400

    h

--- scripts/iDuck.js
+++ scripts/iDuck.js
@@ -8,13 +8,9 @@ const guesses = document.querySelector('.guesses');
 
 
 function checkGuess() {
-    const userGuess = Text(guessField.value);
 
-    guesses.textContent = `${userGuess} `;
-    console.log(guesses.textContent)
-    console.log(userGuess)
    //myJson.person[guesses.textContent]
-    document.getElementById("box").innerText = userGuess;
+    document.getElementById("box").innerText = document.getElementById('textbox_id').value;
 
   }
 
