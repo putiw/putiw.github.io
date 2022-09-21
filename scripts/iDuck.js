5f058a26ee6a4d0e0539a6917cf548ff49781f11
putiwen <pw1246@nyu.edu>
2022-09-21 19:30:09 -0400

    s

--- scripts/iDuck.js
+++ scripts/iDuck.js
@@ -1,5 +1,21 @@
 
 import myJson from './conversation.json' assert {type: 'json'};
 
+const ask = document.querySelector('.ask');
+const ques = document.querySelector('.ques');
 
-document.getElementById("box").innerText = myJson.person["qq"];
+const guesses = document.querySelector('.guesses');
+
+
+function checkGuess() {
+    const userGuess = Text(guessField.value);
+
+    guesses.textContent = `${userGuess} `;
+   
+    document.getElementById("box").innerText = myJson.person[guesses.textContent];
+
+  }
+
+  ask.addEventListener('click', checkGuess);
+
+
