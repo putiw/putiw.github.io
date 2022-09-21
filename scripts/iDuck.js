0f439c26dcb8bc31a5afd021d7e6f35f58225a38
putiwen <pw1246@nyu.edu>
2022-09-21 19:06:14 -0400

    w

--- scripts/iDuck.js
+++ scripts/iDuck.js
@@ -1,7 +1,5 @@
 import myJson from './conversation.json' assert {type: 'json'};
 
-myJson.forEach(function (el, i, arr) {
-    lookup[el.question] = el.answer;
-});
 
-console.log(lookup['Who are you']); 
+
+let result = myJson["Who are you"];
